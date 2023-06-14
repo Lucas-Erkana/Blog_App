0342e5ae76df2700d612c901889d3810a754272f
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-14 16:29:10 +0200

    Update post.rb and user.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  # Updates the posts counter for a user
+  def update_user_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  # Returns the 5 most recent comments for a given post
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes
+
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
+  end
+
+  def update_posts_counter
+    update(posts_counter: posts.count)
+  end
 end
