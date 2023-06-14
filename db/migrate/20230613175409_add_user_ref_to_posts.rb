9680b1a754e5843ea70ddcef6ed3a66c95103e1d
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-14 08:47:05 +0200

    user table refered as author

--- db/migrate/20230613175409_add_user_ref_to_posts.rb
+++ db/migrate/20230613175409_add_user_ref_to_posts.rb
@@ -1,5 +1,6 @@
 class AddUserRefToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :user, null: false, foreign_key: true
+    add_reference :posts, :author, foreign_key: { to_table: 'users' }
   end
 end
+
--- db/migrate/20230613175604_add_post_ref_to_likes.rb
+++ db/migrate/20230613175604_add_post_ref_to_likes.rb
@@ -1,5 +1,5 @@
 class AddPostRefToLikes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :likes, :post, null: false, foreign_key: true
+    add_reference :likes, :author, foreign_key: { to_table: 'users' }
   end
 end
