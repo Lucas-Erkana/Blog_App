9096fea67aa44d06a9f97b8b1535ca2ce11ce284
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-15 11:33:57 +0200

    Add validates
    -Add validation to model/user.rb
    -Add validation to model/post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,14 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  # Updates the posts counter for a user
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comment_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
   end
 
-  # Returns the 5 most recent comments for a given post
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
