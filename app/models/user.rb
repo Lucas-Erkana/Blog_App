4d46fe5d3e7c76f114bb48087771cb02a4ee7fff
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-16 11:19:02 +0200

    Add test for user
    -=Add the following to user.rb
      validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
    
      def recent_post
        posts.order(created_at: :desc).limit(3)
    
    -Create user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  has_many :posts
   has_many :comments
+  has_many :posts
   has_many :likes
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
-  def recent_posts(limit = 3)
-    posts.order(created_at: :desc).limit(limit)
+  def recent_post
+    posts.order(created_at: :desc).limit(3)
   end
 end
