acd83ffce22d2d514ad4bff3af062a1976c4e5d8
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-22 13:08:25 +0200

    Add methods to like and unlike a post

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,16 @@ class User < ApplicationRecord
   def recent_post
     posts.order(created_at: :desc).limit(3)
   end
+
+  def user_has_liked(post_id)
+    likes.exists?(post_id: post_id)
+  end
+
+  def remove_user_like(post_id)
+    likes.find_by(post_id: post_id).destroy
+  end
+
+  def add_user_like(post_id)
+    likes.create(post_id: post_id)
+  end
 end
