2d3ddda66b6f01da30012742623bb2f0e58bbae8
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-22 12:43:27 +0200

    Add add_post functionality
    •	the PostsController handles actions related to displaying, creating, and showing posts. It retrieves the necessary data from the database, assigns it to instance variables, and renders appropriate views or redirects based on the action's outcome.
    •	this code defines the behavior and validations for the Post model, including the update of the posts_counter attribute for the associated user and the retrieval of recent comments for a post.
    •	These helper methods provide reusable code to render post-related elements in the views and enhance code organization and readability.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,31 @@ class PostsController < ApplicationController
     @comments = Comment.all
   end
 
+  def new
+    @current_user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(title: post_params[:title], text: post_params[:text], user_id: current_user[:id],
+                     comment_counter: 0, likes_counter: 0)
+
+    if @post.save
+      redirect_to user_post_path(current_user, @post), notice: 'Post created successfully.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
     @comments = Comment.where(post_id: params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
