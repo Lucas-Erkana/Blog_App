94af748521075159a8e095ab3f68c071f90ae67c
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-26 18:27:35 +0200

    Fix request

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.0.2'
+ruby '3.2.2'
 
 gem 'rubocop', '>= 1.0', '< 2.0'
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,29 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
-  end
-
-  def new
-    @current_user = current_user
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(title: post_params[:title], text: post_params[:text], user_id: current_user[:id],
-                     comment_counter: 0, likes_counter: 0)
-
-    if @post.save
-      redirect_to user_post_path(current_user, @post), notice: 'Post created successfully.'
-    else
-      render :new, status: :unprocessable_entity
-    end
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(params[:user_id])
-    @comments = Comment.where(post_id: params[:id])
+    @current = current_user
   end
 
-  def include_user
-    @user = User.includes(:posts, posts: [:comments, { comments: [:author] }]).find(params[:user_id])
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    if @post.save
+      flash[:success] = 'New post uploaded successfully!'
+      redirect_to user_post_url(current_user, @post)
+    else
+      flash[:error] = 'Post upload failed! Please try again.'
+      redirect_to new_user_post_url(current_user)
+    end
   end
 
   private
