4d805f1b3c3c452399c87ef8220b899b4a5dd601
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-27 17:43:40 +0200

    Fix sin up issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Blog_App_production"
+  # config.active_job.queue_name_prefix = "My_Blog_App_production"
 
   config.action_mailer.perform_caching = false
 
