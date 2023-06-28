8fae330cee20735e641bfb762e1bef42db8d60aa
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-28 16:57:37 +0200

    Update ability and comment models
    Comment out confirmable for testing purposes in the user.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post
   validates_presence_of :text
   after_create :update_comment_counter
+  after_destroy :update_comment_counter
 
   def update_comment_counter
     post.update(comment_counter: post.comments.count)
