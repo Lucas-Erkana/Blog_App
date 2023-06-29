38cef0f8bd3b589e06502ddf25d2eede8f2e0024
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-29 11:59:41 +0200

    Update styling and add Photo url

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable # , :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
+
   has_many :comments
   has_many :posts
   has_many :likes
 
   validates :name, presence: true
+
+  # has_one_attached :photo # Add this line to handle the photo attachment
 
   def recent_post
     posts.order(created_at: :desc).limit(3)
