634df19b9ae79fac0eb8446400a71cbb55c40252
SabaAhmad404 <sabaahmad895@gmail.com>
2023-06-29 11:54:40 +0500

    Add style and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'sign_up', to: 'devise/registrations#new'
@@ -14,12 +9,13 @@ Rails.application.routes.draw do
   devise_for :users, skip: :all
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :show] do
-      resources :comments, only: [:new, :create]
+    resources :posts, only: [:index, :new, :show, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
     end
   end
   post "users/:user_id/posts", to: "posts#create", as: :user_create_post
 end
+
 
 
