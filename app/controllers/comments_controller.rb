e793aad3779c9c5279d133fbc11e647be44ca69d
SabaAhmad404 <sabaahmad895@gmail.com>
2023-06-29 11:56:23 +0500

    updated controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,15 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
+
+
   def new
     @comment = Comment.new
   end
 
+
   def create
     @comment = Comment.new(text: comment_params[:text], post_id: params[:post_id], user_id: current_user[:id])
+
 
     if @comment.save
       redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id]),
@@ -14,9 +19,21 @@ class CommentsController < ApplicationController
     end
   end
 
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id]),
+                notice: 'Comment deleted successfully'
+  end
+
+
   private
+
 
   def comment_params
     params.require(:comment).permit(:text)
   end
 end
+
+
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,23 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
+
 
   def new
     @current_user = current_user
     @post = Post.new
   end
 
+
   def create
     @post = Post.new(title: post_params[:title], text: post_params[:text], user_id: current_user[:id],
                      comment_counter: 0, likes_counter: 0)
+
 
     if @post.save
       redirect_to user_post_path(current_user, @post), notice: 'Post created successfully.'
@@ -20,19 +26,37 @@ class PostsController < ApplicationController
     end
   end
 
+
   def show
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
     @comments = Comment.where(post_id: params[:id])
   end
 
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    redirect_to user_posts_path(current_user, @post), notice: 'Post was successfully destroyed.'
+  end
+
+
   def include_user
     @user = User.includes(:posts, posts: [:comments, { comments: [:author] }]).find(params[:user_id])
   end
 
+
   private
+
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 
   def post_params
     params.require(:post).permit(:title, :text)
   end
 end
+
+
