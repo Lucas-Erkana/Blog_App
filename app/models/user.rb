7e84f98f638ea7aae670d7afe143f65a6ed655b2
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-29 16:46:52 +0200

    Generate Api and Spec folders with files
    -app/controllers/api/api_controller.rb
    -app/controllers/api/comments_controller.rb
    -app/controllers/api/posts_controller.rb
    -app/helpers/api/api_helper.rb
    -app/helpers/api/comments_helper.rb
    -app/helpers/api/posts_helper.rb
    -spec/helpers/api/api_helper_spec.rb
    -spec/helpers/api/comments_helper_spec.rb
    --spec/helpers/api/posts_helper_spec.rb
    -spec/requests/api/api_spec.rb
    -spec/requests/api/comments_spec.rb
    -spec/requests/api/posts_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable # , :confirmable
 
   has_many :comments
   has_many :posts
---
+++ spec/requests/api/api_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+
+RSpec.describe 'Api::Apis', type: :request do
+  describe 'GET /index' do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
